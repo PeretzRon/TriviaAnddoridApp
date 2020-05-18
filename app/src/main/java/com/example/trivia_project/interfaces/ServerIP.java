7dee4ac8368a4d3c8a80fc8214402310c1de8951
ronpe <ron.peretz1@gmail.com>
2020-05-18 10:32:44 +0300

    Update server ip

--- app/src/main/java/com/example/trivia_project/interfaces/ServerIP.java
+++ app/src/main/java/com/example/trivia_project/interfaces/ServerIP.java
@@ -4,5 +4,5 @@ public interface ServerIP {
  //   public String serverIP = "peretzron1.ddns.net";
 //    public String serverIP = "10.0.0.65";
 //    public String serverIP = "192.168.43.40";
-      public String serverIP = "10.0.0.10";
+      public String serverIP = "10.0.0.65";
 }
